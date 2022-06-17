98817a9d84423cf1b996ca12bb28b1b3554ea836
goo-gy <lkukh17@naver.com>
2022-06-17 14:20:38 +0900

    spring boot
    - spring boot로 프로젝트 생성

---
+++ src/main/java/googy/googyspring/GoogySpringApplication.java
@@ -0,0 +1,13 @@
+package googy.googyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/googy/googyspring/GoogySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package googy.googyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
