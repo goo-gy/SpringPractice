1bb8faded6da5887f23cc2e5d80871990ff539ca
goo-gy <lkukh17@naver.com>
2022-06-23 23:39:13 +0900

    API 방식
    - ResponseBody
    - return 값을 그대로 response body에 담아보냄

--- src/main/java/googy/googyspring/controller/GoogyController.java
+++ src/main/java/googy/googyspring/controller/GoogyController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GoogyController {
@@ -17,5 +18,11 @@ public class GoogyController {
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
         model.addAttribute("name", name);
         return "hello-mvc";
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public String helloApi(@RequestParam("name") String name) {
+        return "Hi " + name + "!";
     }
 }
