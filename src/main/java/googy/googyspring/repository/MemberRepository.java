3bdc748c727b73b61b7c6d0a36bc2a7ebf570a7f
goo-gy <lkukh17@naver.com>
2022-06-29 15:32:46 +0900

    Member Repository 인터페이스 작성

---
+++ src/main/java/googy/googyspring/repository/MemberRepository.java
@@ -0,0 +1,15 @@
+package googy.googyspring.repository;
+
+import googy.googyspring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+
+}
