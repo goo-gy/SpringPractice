3cc90e9a9acc5e449086b480946e6c439a3ef0d3
goo-gy <lkukh17@naver.com>
2022-06-30 20:31:11 +0900

    service 작성

---
+++ src/main/java/googy/googyspring/service/MemberService.java
@@ -0,0 +1,38 @@
+package googy.googyspring.service;
+
+import googy.googyspring.domain.Member;
+import googy.googyspring.repository.MemberRepository;
+import googy.googyspring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        checkDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void checkDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName()).ifPresent(mem -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
