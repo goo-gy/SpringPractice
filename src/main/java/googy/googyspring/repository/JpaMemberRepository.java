219e6e8e5e155feea46dd3636bb44d542d9a8664
goo-gy <lkukh17@naver.com>
2022-07-10 16:41:15 +0900

    FEAT : JPA Member
    - Domain을 Entity로 설정
    - JpaMemberRepository 작성
    - Service에 Transactional 추가

---
+++ src/main/java/googy/googyspring/repository/JpaMemberRepository.java
@@ -0,0 +1,42 @@
+package googy.googyspring.repository;
+
+import googy.googyspring.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class JpaMemberRepository implements MemberRepository{
+    private final EntityManager entityManager;
+
+    public JpaMemberRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Member save(Member member) {
+        entityManager.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = entityManager.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result =  entityManager.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return entityManager.createQuery("SELECT m from Member m", Member.class).getResultList();
+    }
+}
